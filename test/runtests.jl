264bc229fd16f61127cf651ef7ff1be132cf05a0
Valentin Churavy <vchuravy@users.noreply.github.com>
2017-09-19 09:19:18 -0400

    Reorder checkout and clone
    so that we don't pull in transitive dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,15 +3,16 @@ if (
         get(ENV, "APPVEYOR", "") == "true" ||
         get(ENV, "CI", "") == "true"
     )
-    Pkg.clone("Transpiler")
-    Pkg.clone("Sugar")
-    Pkg.clone("GPUArrays")
     Pkg.clone("OpenCL")
+    Pkg.clone("Suger")
+    Pkg.clone("Transpiler")
+    Pkg.clone("GPUArrays")
 
-    Pkg.checkout("Transpiler", "sd/struct_buffer")
-    Pkg.checkout("Sugar", "sd/struct_buffer")
-    Pkg.checkout("GPUArrays", "sd/abstractgpu")
     Pkg.checkout("OpenCL", "sd/pointerfree")
+    Pkg.checkout("Sugar", "sd/struct_buffer")
+    Pkg.checkout("Transpiler", "sd/struct_buffer")
+    Pkg.checkout("GPUArrays", "sd/abstractgpu")
+
 end
 
 using CLArrays
